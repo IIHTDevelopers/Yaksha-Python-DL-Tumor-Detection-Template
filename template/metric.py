d64cbe16677842c91a6f5053ff96bba358747fe0
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-04-29 15:45:48 +0530

    Delete template directory

--- template/metric.py
+++
@@ -1,4 +0,0 @@
-## write the code for calculating dice cofficient. Firstly flatten the inputs and targets tensor and calculate the dice score.
-def dice_cofficient(inputs, targets, smooth=1):
-    ## Write code here and return type will be a tensor having the dice score
-    pass
