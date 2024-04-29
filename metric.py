0688b6008266cb8b1400bec5e923c69b4e1d1052
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-04-29 15:47:23 +0530

    Delete metric.py

--- metric.py
+++
@@ -1,8 +0,0 @@
-def dice_cofficient(inputs, targets, smooth=1):
-    inputs = inputs.view(-1)
-    targets = targets.view(-1)
-
-    intersection = (inputs * targets).sum()
-    dice = (2.*intersection + smooth)/(inputs.sum() + targets.sum() + smooth)
-
-    return dice
